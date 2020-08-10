855f3cf98f20b553c92322445c215b8fdac976d6
Nick Lanese <nlanese21@gmail.com>
2020-08-10 03:00:43 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,4 +2,22 @@ class Author
 
   attr_accessor :name
 
+  @@allPosts = 0
+
+  def initialize(name)
+    @name = name
+    posts =[]
+  end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    @posts << post
+    if (post.author != self)
+      post.author = self
+    end
+  end
+
 end
