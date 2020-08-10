fbdbe93777b7f0b3b0088daf00678e0f1497df2d
Nick Lanese <nlanese21@gmail.com>
2020-08-10 03:03:28 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,4 +1,17 @@
 class Post
 
   @@all = []
+
+  def title=(input)
+    @title = input
+  end
+
+  def title
+    @title
+  end
+
+  def initialize(title)
+    @title = title
+    @@all << self
+  
 end
